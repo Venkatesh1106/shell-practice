eaa82c08ddcadd33260a99cd8389cdd8390e628b
venkatesh <venkatesh1106@gmail.com>
2025-09-27 12:16:53 -0400

    shell practice

---
+++ 09-data-types.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#everything is shell considered as string, durig execution it 
+#will take as numeric
+NUMBER1=100
+NUMBER2=200
+
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "Total value is ${SUM}"
