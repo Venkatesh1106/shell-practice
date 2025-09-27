21727e838fff71211b5db57142d2432e3b58d361
venkatesh <venkatesh1106@gmail.com>
2025-09-27 12:25:35 -0400

    shell practice

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -8,5 +8,7 @@ SUM=$(($NUMBER1+$NUMBER2))
 
 echo "Total value is ${SUM}"
 
+#SIZE of Array 3 index is 2
 LEADERS=("Modi" "Putin" "Trump")
 echo "All Leaders : ${LEADERS[@]}"
+echo "First Leaders : ${LEADERS[0]}"
